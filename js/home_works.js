8afdbf2949fcda105e977bff5f9a300702a3b437
taranovpavel <155109498+taranovpavel@users.noreply.github.com>
2024-01-13 09:01:50 +0600

    dz2

--- js/home_works.js
+++ js/home_works.js
@@ -13,14 +13,50 @@ gmailButton.onclick = () =>{
     }
 }
 const box = document.querySelector(".child_block")
-let count = 1
-const increment = () => {
-    count ++
-    box.style.left = `${count}px`
-    console.log(count)
-    if (count <448){
-        window.requestAnimationFrame(increment)
+let x = 0
+let y = 0
+const max = 448
+const move = () =>{
+    if ((x !== max)&&(y === 0)){
+        x++
+        box.style.left = `${x}px`
+        requestAnimationFrame(move)
+    }else if ((y !== max)&&(x === max)){
+        y++
+        box.style.top = `${y}px`
+        requestAnimationFrame(move)
+    }else if ((y === max)&&(x > 0)){
+        x--
+        box.style.left = `${x}px`
+        requestAnimationFrame(move)
+    }else if ((x !== max)&&(y > 0)){
+        y--
+        box.style.top = `${y}px`
+        requestAnimationFrame(move)
     }
 }
-window.requestAnimationFrame(increment)
+move()
 
+const seconds = document.querySelector("#seconds")
+const start = document.querySelector("#start")
+const stop = document.querySelector("#stop")
+const reset = document.querySelector("#reset")
+let i = 0
+let b = 0
+let c = 0
+
+
+const timer = () =>{
+    if ((b === 0)&&(c <= 1)) {
+        i++
+        seconds.innerText = i
+        setTimeout(timer, 1000)
+    } else {
+        b = 0
+        c --
+    }
+}
+start.addEventListener("click",start_func = () =>{c++})
+start.addEventListener("click",timer,)
+stop.addEventListener("click",stop_func = () =>{b++;c=1;})
+reset.addEventListener("click",reset_func = () =>{i=0;b++;seconds.innerText= i})
